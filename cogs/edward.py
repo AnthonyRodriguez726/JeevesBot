c6efcdc712d8b47a491ce0c599b4dd28fd9c4e1d
Anthony Rodriguez <Anthony Rodriguez>
2017-02-24 03:23:16 -0500

    Added urban dictionary search. Fixed problem with methods that had more than 1 argument and also used consume rest.

--- cogs/edward.py
+++ cogs/edward.py
@@ -1,5 +1,7 @@
 import discord
 from discord.ext import commands
+import random
+import os
 
 class Edward():
 	def __init__(self, bot):
@@ -8,20 +10,21 @@ class Edward():
 	@commands.command(pass_context=True)
 	async def edward(self, ctx):
 		channel = self.bot.get_channel(ctx.message.channel.id)
-		await self.bot.send_file(channel, "images/edward.png")
-		await self.bot.delete_message(ctx.message)
+		edwards = os.listdir('images/edward/')
+		choice = random.choice(edwards)
+		await self.bot.send_file(channel, "images/edward/"+choice)
+		
 
-	@commands.command(pass_context=True)
-	async def edward_pls(self, ctx):
-		channel = self.bot.get_channel(ctx.message.channel.id)
-		await self.bot.send_file(channel, "images/edward_pls.png")
-		await self.bot.delete_message(ctx.message)
+	# @commands.command(pass_context=True)
+	# async def edward_pls(self, ctx):
+	# 	channel = self.bot.get_channel(ctx.message.channel.id)
+	# 	await self.bot.send_file(channel, "images/edward_pls.png")
 
-	@commands.command(pass_context=True)
-	async def EDWARD(self, ctx):
-		channel = self.bot.get_channel(ctx.message.channel.id)
-		await self.bot.send_file(channel, "images/EDWARD!.png")
-		await self.bot.delete_message(ctx.message)
+	# @commands.command(pass_context=True)
+	# async def EDWARD(self, ctx):
+	# 	channel = self.bot.get_channel(ctx.message.channel.id)
+	# 	await self.bot.send_file(channel, "images/EDWARD!.png")
+	# 	await self.bot.delete_message(ctx.message)
 
 def setup(bot):
 	bot.add_cog(Edward(bot))
